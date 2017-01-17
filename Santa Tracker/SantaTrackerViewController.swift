c671a946ff3e975aceda2e7a07378206a7c1f53a
anseee <admin@ansee.kr>
2017-01-17 23:51:02 +0900

    프로퍼티 추가.

--- Santa Tracker/SantaTrackerViewController.swift
+++ Santa Tracker/SantaTrackerViewController.swift
@@ -7,9 +7,16 @@
 //
 
 import UIKit
+import MapKit
 
 class SantaTrackerViewController: UIViewController {
 
+    @IBOutlet private weak var timeRemainingLabel: UILabel!
+    @IBOutlet private weak var mapView: MKMapView!
+    @IBOutlet private weak var activityLabel: UILabel!
+    @IBOutlet private weak var temperatureLabel: UILabel!
+    @IBOutlet private weak var presentsRemainingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,6 @@ class SantaTrackerViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
--- Santa TrackerTests/Santa_TrackerTests.swift
+++ Santa TrackerTests/Santa_TrackerTests.swift
@@ -7,7 +7,6 @@
 //
 
 import XCTest
-@testable import Santa_Tracker
 
 class Santa_TrackerTests: XCTestCase {
     
@@ -27,10 +26,7 @@ class Santa_TrackerTests: XCTestCase {
     }
     
     func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        
     }
     
 }
