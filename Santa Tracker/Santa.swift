2912382b7baa24d3b7c98cd3381ada3305a588b8
anseee <admin@ansee.kr>
2017-01-24 23:06:42 +0900

    Santa.swift -> 옵셔널이면 무조건 초기화 -> 아니면 새로운 값으로 설정

--- Santa Tracker/Santa.swift
+++ Santa Tracker/Santa.swift
@@ -10,5 +10,20 @@ import UIKit
 import RealmSwift
 
 class Santa: Object {
-    dynamic var currentLocation: Location?
+    dynamic var _currentLocation: Location?
+    
+    var currentLocation: Location {
+        get {
+            // 산타가 어디있는지 알지 못한다면, 산타는 여전히 그의 집에 있을 것으로 추정됩니다.
+            return _currentLocation ?? Location(latitude: 90, longitude: 180)
+        }
+        set {
+            _currentLocation = newValue
+        }
+    }
+    
+    //  또한, Realm 이 currentLocation 속성을 저장하지 않도록 하기 위해 ignoredProperties 를 사용했습니다.
+    override static func ignoredProperties() -> [String] {
+        return ["currentLocation"]
+    }
 }
